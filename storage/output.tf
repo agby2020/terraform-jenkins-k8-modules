0487505c9246e34389c2bb36ad3a8c2938fc9e2b
Anton Plyhauka <Anton.Plyhauka@gmail.com>
2024-01-08 17:29:49 +0100

    Fixed Storage Outputs

--- storage/output.tf
+++ storage/output.tf
@@ -7,8 +7,3 @@ output "pvc_namespace" {
   description = ""
   value = kubernetes_persistent_volume_claim.jenkins_pv_claim.metadata[0].namespace
 }
-
-output "pvc_accessmode" {
-  description = ""
-  value = kubernetes_persistent_volume_claim.jenkins_pv_claim.access_modes
-}
